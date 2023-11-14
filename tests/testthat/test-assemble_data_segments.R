eae188a5bd2a79cef58a2aac24040d429ae560f8
Gleb Ebert <5892266+gl-eb@users.noreply.github.com>
2023-11-14 17:49:43 +0100

    expand unit testing

--- tests/testthat/test-assemble_data_segments.R
+++ tests/testthat/test-assemble_data_segments.R
@@ -1,10 +1,4 @@
-test_that("parser expects file path to be a string", {
-  expect_error(assemble_data_segments(3))
-})
-
-test_that("parser expects valid file path", {
-  expect_error(assemble_data_segments("test.xlsx"))
-})
+# test correct functioning of assemble_data_segments() -------------------------
 
 test_that("function should run without errors", {
   expect_no_error(
@@ -13,8 +7,30 @@ test_that("function should run without errors", {
       "tecan_time_series_segments.xlsx",
       package = "tecanr"
     ) |>
-    assemble_data_segments()
+    assemble_data_segments() |>
+    suppressMessages()
   )
+})
+
+test_that("tecan_parse's messages should be printed", {
+  expect_message(
+    system.file(
+      "extdata",
+      "tecan_time_series_segments.xlsx",
+      package = "tecanr"
+    ) |>
+    assemble_data_segments(),
+    "Multiple reads"
+  ) |> suppressMessages()
+  expect_message(
+    system.file(
+      "extdata",
+      "tecan_time_series_segments.xlsx",
+      package = "tecanr"
+    ) |>
+    assemble_data_segments(),
+    "Time series"
+  ) |> suppressMessages()
 })
 
 test_that("output should be tibble", {
@@ -24,7 +40,74 @@ test_that("output should be tibble", {
       "tecan_time_series_segments.xlsx",
       package = "tecanr"
     ) |>
-    assemble_data_segments(),
+    assemble_data_segments() |>
+    suppressMessages(),
     "tbl_df"
+  )
+})
+
+
+# test detection of non-compliant arguments ------------------------------------
+
+test_that("file path should be string", {
+  expect_error(
+    assemble_data_segments(3),
+    "must be a character"
+  )
+})
+
+test_that("file path should be valid", {
+  expect_error(
+    assemble_data_segments("test.xlsx"),
+    "does not exist"
+  )
+})
+
+test_that("expect segments <= sheets", {
+  expect_error(
+    system.file(
+      "extdata",
+      "tecan_time_series_segments.xlsx",
+      package = "tecanr"
+    ) |>
+    assemble_data_segments(segments = 4),
+    "`segments` is greater"
+  )
+})
+
+test_that("number of segments should be >= 1", {
+  expect_error(
+    system.file(
+      "extdata",
+      "tecan_time_series_segments.xlsx",
+      package = "tecanr"
+    ) |>
+    assemble_data_segments(segments = 0),
+    "`segments` must be >= 1"
+  )
+  expect_error(
+    system.file(
+      "extdata",
+      "tecan_time_series_segments.xlsx",
+      package = "tecanr"
+    ) |>
+    assemble_data_segments(segments = -3),
+    "`segments` must be >= 1"
+  )
+})
+
+test_that("sheets with invalid data should be ignored", {
+  expect_no_error(
+    test_path("fixtures", "time_series_invalid_sheet.xlsx") |>
+    assemble_data_segments() |>
+    suppressMessages()
+  )
+})
+
+test_that("segments should not overlap in time", {
+  expect_error(
+    test_path("fixtures", "time_series_negative_offset.xlsx") |>
+    assemble_data_segments() |>
+    suppressMessages()
   )
 })
